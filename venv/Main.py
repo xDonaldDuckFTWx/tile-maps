4a1db854f14977216720d06d1cac703d14c36bfc
Olle Lapidus <olllap0129@hotmail.com>
2020-07-04 13:48:26 +0200

    country maps

--- venv/Main.py
+++ venv/Main.py
@@ -1,6 +1,7 @@
 from Generate import *
 from DrawSavedMap import *
 from CreateMap import *
+import os
 
 
 def main():
@@ -14,7 +15,16 @@ def main():
         return "escape"
     
 def browse():
-    maps = ["world", "europe", "africa", "asia", "american_continent", "latin_america", "china_provinces",
+    rootdir = __file__.replace("Main.py", "") + "maps/final_maps"
+    # 'C:/Users/olle.lapidus/PycharmProjects/tile-maps/venv/maps/final_maps'
+    maps = []
+    mapfiles = []
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            mapfiles.append(os.path.join(subdir, file))
+            maps.append(subdir.replace(rootdir, "")[1:] + "_" + file.replace(".json", ""))
+
+    """maps = ["world", "europe", "africa", "asia", "american_continent", "latin_america", "china_provinces",
             "usa_states", "india_provinces", "sweden_counties", "sweden_municipalities", "european_union_member_states",
             "europe_full", "american_continent_full", "world_full", "france_regions", "spain_provinces", "brazil_states",
             "denmark_municipalities", "austria_states", "ireland_counties", "greece_regions", "turkey", "italy_provinces",
@@ -22,7 +32,7 @@ def browse():
             "mexico_states", "oman_provinces", "oman_regions", "pakistan_regions", "poland_regions", "portugal_regions",
             "romania_regions", "serbia_regions", "south_africa_regions", "spain_communities", "netherlands_regions",
             "england_regions"]
-    mapfiles = ["maps/final_maps/{}.json".format(i) for i in maps]
+    mapfiles = ["maps/final_maps/{}.json".format(i) for i in maps]"""
     browsing_index = 0
 
     while True:
